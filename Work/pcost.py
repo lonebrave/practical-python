aa8446123ab7fce293b85a8f8eea8bb8a6b513ab
Nick Hasser <nick.hasser@gmail.com>
2020-06-29 19:03:53 +0100

    exercise 1.27 complete

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Data/portfolio.csv', 'rt') as f:
+    data = f.read()
+
+mylist = data.split()
+
+del mylist[0]
+
+totalcost = 0
+for item in mylist:
+    symbol, shares, cost = item.split(',')
+    totalcost += float(shares) * float(cost)
+
+print('Total cost: {}'.format(totalcost))
