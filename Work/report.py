a14a429737b455b5c1eaceb07e245b64397e2bef
Nick Hasser <nick.hasser@gmail.com>
2020-06-30 18:00:42 +0100

    exercise 2.5 complete

--- Work/report.py
+++ Work/report.py
@@ -11,7 +11,10 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {'name': row[0],
+                       'shares': int(row[1]),
+                       'price': float(row[2])
+                       }
             portfolio.append(holding)
 
     return portfolio
