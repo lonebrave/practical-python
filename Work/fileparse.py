d7c2f3af825ddf7f861c2051be0e35b73230a42d
Nick Hasser <nick.hasser@gmail.com>
2020-07-04 13:59:22 +0100

    adding support for files without headers
    exercise 3.6 complete

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,7 +4,7 @@
 import csv
 
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, select=None, types=None, has_headers=True):
     '''
     Parse a CSV file into a list of records
     '''
@@ -12,32 +12,45 @@ def parse_csv(filename, select=None, types=None):
     with open(filename) as f:
         rows = csv.reader(f)
 
-        # Read the file headers
-        headers = next(rows)
         records = []
 
-        # Use select fields if specified, otherwise use all headers from file
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
+        if has_headers:
+            # Read the file headers
+            headers = next(rows)
+
+            # Use select fields if specified, otherwise use all headers from
+            # file
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = []
+
+            for row in rows:
+                if not row:     # Skip rows with no data
+                    continue
+
+                # Only use selected fields, if specified
+                if indices:
+                    row = [row[index] for index in indices]
+
+                # Apply type conversion, is specified
+                if types:
+                    row = [func(val) for func, val in zip(types, row)]
+
+                # Create dictionary
+                record = dict(zip(headers, row))
+
+                records.append(record)
         else:
-            indices = []
+            for row in rows:
+                if not row:     # Skip rows with no data
+                    continue
 
-        for row in rows:
-            if not row:     # Skip rows with no data
-                continue
+                record = ()
+                for index, val in enumerate(row):
+                    record += (types[index](val),)
 
-            # Only use selected fields, if specified
-            if indices:
-                row = [row[index] for index in indices]
-
-            # Apply type conversion, is specified
-            if types:
-                row = [func(val) for func, val in zip(types, row)]
-
-            # Create dictionary
-            record = dict(zip(headers, row))
-
-            records.append(record)
+                records.append(record)
 
     return records
