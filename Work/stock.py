3b7a05b0cfbb1197d5de13493df3d7646c27cc6d
Nick Hasser <nick.hasser@gmail.com>
2020-07-05 18:14:20 +0100

    create Stock class
    exercise 4.1 complete

---
+++ Work/stock.py
@@ -0,0 +1,8 @@
+class Stock:
+    '''
+    Class representing a holding of stock.
+    '''
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
