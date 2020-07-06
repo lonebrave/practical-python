292f737ef531400db25fdd574d93ce8285cbca09
Nick Hasser <nick.hasser@gmail.com>
2020-07-06 11:00:29 +0100

    adding __repr__
    exercise 4.9 complete

--- Work/stock.py
+++ Work/stock.py
@@ -7,6 +7,10 @@ class Stock:
         self.shares = shares
         self.price = price
 
+    def __repr__(self):
+        # print(f'Stock({self.name},{self.shares},{self.price})')
+        return f"Stock('{self.name}',{self.shares},{self.price})"
+
     def cost(self):
         return self.shares * self.price
 
