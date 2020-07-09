059bc8a301e3590d8c07c5bc18ea7a02069b30b5
Nick Hasser <nick.hasser@gmail.com>
2020-07-09 18:31:34 +0100

    packaging
    exercise 9.5 complete

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name='porty',
+    version='0.0.1',
+    author='Nick Hasser',
+    author_email='nick.hasser@gmail.com',
+    description='Practical Python Code',
+    packages=setuptools.find_packages(),
+)
